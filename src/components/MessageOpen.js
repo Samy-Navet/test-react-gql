8ebf4aceb0bdbe351546e6fccc1830d5b3b582ca
Navet <samynavet@gmail.com>
2019-10-16 14:20:31 +0200

    amélioration cercle svg

--- src/components/MessageOpen.js
+++ src/components/MessageOpen.js
@@ -44,8 +44,8 @@ const MessageOpen = (props) => {
                 <div className="msg-header">
                     <div className="msg-open-svg_wrapper">
                         <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="20" cy="20" r="20" fill="#049FAF"/>
-                            <path d="M23.454 23.242H15.68L13.86 28H11.676L18.358 10.84H20.828L27.484 28H25.274L23.454 23.242ZM22.7 21.292L20.386 15.208L19.58 12.816H19.528L18.774 15.13L16.434 21.292H22.7Z" fill="white"/>
+                            <circle cx="20" cy="20" r="20" fill="#049FAF" />
+                                <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle"  fill="white"> {data.message.username.charAt(0)} </text>
                         </svg>
                     </div>
                     <div>{data.message.username}</div>
